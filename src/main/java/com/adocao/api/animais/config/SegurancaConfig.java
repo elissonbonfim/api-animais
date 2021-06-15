13c8f6621a599a016017ee01c5ca6e2ad441974e
elisson <elisson@elisson-PC>
2021-06-15 14:51:44 -0300

    1.0 Project. 	1.1 Adição Segurança.

---
+++ src/main/java/com/adocao/api/animais/config/SegurancaConfig.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adocao.api.animais.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ *
+ * @author elisson
+ */
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SegurancaConfig extends WebSecurityConfigurerAdapter{
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("elisson").password("bonfim").roles("ADMIN");
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  NoOpPasswordEncoder.getInstance();
+    }
+
+}
--- src/main/java/com/adocao/api/animais/controllers/AnimaisController.java
+++ src/main/java/com/adocao/api/animais/controllers/AnimaisController.java
@@ -8,20 +8,14 @@ package com.adocao.api.animais.controllers;
 import com.adocao.api.animais.entities.AnimaisJPA;
 import com.adocao.api.animais.exceptions.AnimaisNotFoundException;
 import com.adocao.api.animais.repositories.AnimaisRepository;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
